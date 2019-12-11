89e523d32089272fc0f0588fcf619de1a57cede4
ndman <ndman@outlook.com>
2019-12-11 13:52:41 +0100

    Added project Example1

---
+++ src/Example1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Example1.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Example1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(
+            IConfiguration configuration,
+            ILogger<HomeController> logger)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public IActionResult Index()
+        {
+            var configurations = new Configurations
+            {
+                Example1 = new Models.Example1
+                {
+                    SiteConfiguration = new SiteConfiguration
+                    {
+                        BaseUrl = _configuration["Example1:SiteConfiguration:BaseUrl"]
+                    }
+                }
+            };
+            return View(configurations);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
